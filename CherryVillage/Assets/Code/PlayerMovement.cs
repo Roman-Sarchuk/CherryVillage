de3aea9ca298ca8944a7ca24115626f40ccd01b6
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2024-12-06 20:44:14 +0200

    🥽 developing deapth moving

--- CherryVillage/Assets/Code/PlayerMovement.cs
+++ CherryVillage/Assets/Code/PlayerMovement.cs
@@ -1,44 +1,79 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _runSpeed = 5f; // Running speed.
-    [SerializeField] private float _jumpForce = 5f; // Jump height.
+    [Header("Base Movement")]
+    [SerializeField] private float _runSpeed = 5f;
+    [SerializeField] private float _jumpForce = 5f;
 
-    [SerializeField] private Sprite _jumpSprite; // Sprite that shows up when the character is not on the ground. [OPTIONAL]
-
-    private Rigidbody2D _rb; // Variable for the RigidBody2D component.
-    [SerializeField] private SpriteRenderer _sprite; // Variable for the SpriteRenderer component.
-    // private Animator animator; // Variable for the Animator component. [OPTIONAL]
-
-    private bool isGrounded; // Variable that will check if character is on the ground.
+    private bool isGrounded;
     [SerializeField] private GameObject _groundCheckPoint; // The object through which the isGrounded check is performed.
-    [SerializeField] private float _groundCheckRadius; // isGrounded check radius.
+    [SerializeField] private float _groundCheckRadius;
     [SerializeField] private LayerMask _groundLayer; // Layer wich the character can jump on.
 
-    private bool _jumpPressed = false; // Variable that will check is "Space" key is pressed.
-    private bool _APressed = false; // Variable that will check is "A" key is pressed.
-    private bool _DPressed = false; // Variable that will check is "D" key is pressed.
+    [Header("Keys")]
+    [SerializeField] private bool _jumpPressed = false; // Variable that will check is "Space" key is pressed.
+    [SerializeField] private bool _APressed = false; // Variable that will check is "A" key is pressed.
+    [SerializeField] private bool _DPressed = false; // Variable that will check is "D" key is pressed.
+    [SerializeField] private bool _intoPressed = false; // Variable that will check is "W" key is pressed.
+    [SerializeField] private bool _outPressed = false; // Variable that will check is "S" key is pressed.
 
-    void Awake()
+    [Header("DepthMovement")]
+    [Range(0f, 100f)]
+    [SerializeField] private float _minScalePercent = 50f;
+    [Range(100f, 200f)]
+    [SerializeField] private float _maxScalePercent = 100f;
+    [SerializeField] private float _scaleCoef = 0.1f;
+    [SerializeField] private Vector3 _scaleVector3;
+    [SerializeField] private Vector3 _initialScale;
+    [SerializeField] private Vector3 _maxScale;
+    [SerializeField] private Vector3 _minScale;
+    [SerializeField] private bool _isDeapthMoving = false;
+    [SerializeField] private bool _isAbleMoveInto;
+    [SerializeField] private bool _isAbleMoveOut;
+    [SerializeField] private LayerMask _upZoneLayer;
+    [SerializeField] private LayerMask _downZoneLayer;
+
+
+    [Header("Other")]
+    [SerializeField] private SpriteRenderer _sprite; // Variable for the SpriteRenderer component.
+    [SerializeField] private Sprite _jumpSprite; // Sprite that shows up when the character is not on the ground. [OPTIONAL]
+    private Rigidbody2D _rb;
+    // private Animator animator; // Variable for the Animator component. [OPTIONAL]
+
+    private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>(); // Setting the RigidBody2D component.
+        _rb = GetComponent<Rigidbody2D>();
+
+        _initialScale = transform.localScale;
+        _maxScale = _initialScale * _maxScalePercent;
+        _minScale = _initialScale * _minScalePercent;
+
+        _scaleVector3 = Vector3.zero * _scaleCoef;
     }
 
-    // Update() is called every frame.
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) _jumpPressed = true; // Checking on "Space" key pressed.
-        if (Input.GetKey(KeyCode.A)) _APressed = true; // Checking on "A" key pressed.
-        if (Input.GetKey(KeyCode.D)) _DPressed = true; // Checking on "D" key pressed.
+        _jumpPressed = Input.GetKeyDown(KeyCode.Space);
+        _APressed = Input.GetKey(KeyCode.A);
+        _DPressed = Input.GetKey(KeyCode.D);
+        _intoPressed = Input.GetKey(KeyCode.W);
+        _outPressed = Input.GetKey(KeyCode.S);
     }
 
-    // Update using for physics calculations.
-    void FixedUpdate()
+    private void FixedUpdate()
+    {
+        if (!_isDeapthMoving)
+            BaseMovement();
+        DepthMovement();
+    }
+
+    private void BaseMovement()
     {
         isGrounded = Physics2D.OverlapCircle(_groundCheckPoint.transform.position, _groundCheckRadius, _groundLayer); // Checking if character is on the ground.
 
-        // Left/Right movement.
+        // Left/Right movement
         if (_APressed)
         {
             _rb.velocity = new Vector2(-_runSpeed, _rb.velocity.y); // Move left physics.
@@ -47,9 +82,9 @@ public class Movement : MonoBehaviour
         }
         else if (_DPressed)
         {
-            _rb.velocity = new Vector2(_runSpeed, _rb.velocity.y); // Move right physics.
+            _rb.velocity = new Vector2(_runSpeed, _rb.velocity.y);
             _sprite.flipX = true; // Rotating the character object to the right.
-            _DPressed = false; // Returning initial value.
+            _DPressed = false;
         }
         else _rb.velocity = new Vector2(0, _rb.velocity.y);
 
@@ -57,7 +92,7 @@ public class Movement : MonoBehaviour
         if (_jumpPressed && isGrounded)
         {
             _rb.velocity = new Vector2(0, _jumpForce); // Jump physics.
-            _jumpPressed = false; // Returning initial value.
+            _jumpPressed = false;
         }
 
         // Setting jump sprite. [OPTIONAL]
@@ -67,5 +102,27 @@ public class Movement : MonoBehaviour
             sr.sprite = jumpSprite; // Setting the sprite.
         }
         else animator.enabled = true; // Turning on animation.*/
+    }
+
+    private void DepthMovement()
+    {
+        _isAbleMoveInto = Physics2D.OverlapCircle(_groundCheckPoint.transform.position, _groundCheckRadius, _upZoneLayer);
+        _isAbleMoveOut = Physics2D.OverlapCircle(_groundCheckPoint.transform.position, _groundCheckRadius, _downZoneLayer);
+
+        // Into/Out movement
+        if (_isAbleMoveInto && _intoPressed)
+        {
+            transform.localScale = transform.localScale + _scaleVector3;
+            //_rb.velocity = new Vector2(-_runSpeed, _rb.velocity.y); // Move left physics.
+            //_sprite.flipX = false; // Rotating the character object to the left.
+            _isDeapthMoving = true;
+        }
+        else if (_isAbleMoveOut && _outPressed)
+        {
+            transform.localScale -= _scaleVector3;
+            //_rb.velocity = new Vector2(_runSpeed, _rb.velocity.y);
+            //_sprite.flipX = true; // Rotating the character object to the right.
+            _isDeapthMoving = true;
+        }
     }
 }
